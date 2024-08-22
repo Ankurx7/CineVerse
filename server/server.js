249388a5d0fef624a3d3ef16d80314e16a0e725b
Ankur Akash <07ankur007@gmail.com>
2024-08-22 15:09:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+}));
 app.use("/", IndexRoute);
 
 // Serve static files
